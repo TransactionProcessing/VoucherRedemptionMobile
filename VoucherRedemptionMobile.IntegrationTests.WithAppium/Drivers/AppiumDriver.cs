0ec10c620aebe6b5f333ca744dfbabd6a229946f
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2021-11-20 09:35:36 +0000

    ...

--- VoucherRedemptionMobile.IntegrationTests.WithAppium/Drivers/AppiumDriver.cs
+++ VoucherRedemptionMobile.IntegrationTests.WithAppium/Drivers/AppiumDriver.cs
@@ -77,7 +77,7 @@ namespace VoucherRedemptionMobile.IntegrationTests.WithAppium.Drivers
                 var driverOptions = new AppiumOptions();
                 driverOptions.AddAdditionalCapability(MobileCapabilityType.PlatformName, "iOS");
                 driverOptions.AddAdditionalCapability(MobileCapabilityType.DeviceName, "iPhone 11");
-                driverOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "15.0");
+                driverOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "14.4");
 
                 String assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
                 String binariesFolder = Path.Combine(assemblyFolder, "..", "..", "..", "..", @"VoucherRedemptionMobile.iOS/bin/iPhoneSimulator/Release");
