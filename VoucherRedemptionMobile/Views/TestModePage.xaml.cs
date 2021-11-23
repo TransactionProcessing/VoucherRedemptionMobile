c5ab38c7b5b0a100de8e669da196a8fc728796e7
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2021-11-23 16:21:51 +0000

    .

--- VoucherRedemptionMobile/Views/TestModePage.xaml.cs
+++ VoucherRedemptionMobile/Views/TestModePage.xaml.cs
@@ -55,9 +55,9 @@ namespace VoucherRedemptionMobile.Views
         {
             this.SetTestModeButton.Clicked += SetTestModeButton_Clicked;
             this.ViewModel = viewModel;
-            this.ViewModel.TestUserData = "i65W8ixJzTVUslIqSk1JzS0oyczPKy1OLXIoSS0uQQgZ6iXn65VmK+mAlRsBlRsaGZuYminVxgIA";
-            this.ViewModel.TestVoucherData =
-                "zZVNaxsxEIb/y17rMRpp9OVTaRpKDrkEk0vpYSSNWlPHDrtraCn575WdxjZJycG0JsvCrsbz6lmsB+nzr+4D93ldpJt13aS7HEYe5aq0kSGfQy4EjJGAEC2wcRXYW7aiKVNMLXGxXo0953GXqdmIrcaBJWagxBpCqgGsichZKBXaZi5/3C/6nx8bqWW00gioQZs52pnBmfZTrR05/06pmVKt/2r4JCvpW3+DjP1GtqWrYdgcj2+kiNxtK5WXw660bdhS5ou7AwkPJKN3JI970p7zWuzPBx7FnthHKaUUwu6e77oeG69lGPjrnz/7RvLifiGr8fKOF8tWG2UY+6civt+/TvN6uvl+nLhep8WyTbPaLJeTbt7zamhrsFivdsuAYslggyfRDih7gVSRwEVlvZdQnLg22y0vN20OVFOlJs2DJa/yYXy73uRv0l88uqGeLuz2P+1YmorSQVlA4saSWiCg9eCIU0QVQ0XdPUzerGfemzN55j2d4tlx7H94pk/3TAdrKJoAFdkCsTAEGyuQt1HnZG3CfPBMP/NMv+qZfuaZD5RD0hlMVgnIKA+hNM8CFsfZBm4avWXPIoUzeRbtKZpFiv9Ys0dnXqjUoTZknQ+xe2lUFmYpqoJqD6CAHjgGAV8zOpcl6qRP3bnMM6MyGW22rFCk2UuOmwvGQpOiFEKfioQ3bJRXNp7HKK/cCUYdp84jVPiLUNUFZaRGsJzbGqmMwDU58M7Z7DKHEvnULYqeCZVCbadutNCOX91YbZ+KkTzkbIzT0aZ2VHYPX34D";
+            //this.ViewModel.TestUserData = "i65W8ixJzTVUslIqSk1JzS0oyczPKy1OLXIoSS0uQQgZ6iXn65VmK+mAlRsBlRsaGZuYminVxgIA";
+            //this.ViewModel.TestVoucherData =
+            //    "zZVNaxsxEIb/y17rMRpp9OVTaRpKDrkEk0vpYSSNWlPHDrtraCn575WdxjZJycG0JsvCrsbz6lmsB+nzr+4D93ldpJt13aS7HEYe5aq0kSGfQy4EjJGAEC2wcRXYW7aiKVNMLXGxXo0953GXqdmIrcaBJWagxBpCqgGsichZKBXaZi5/3C/6nx8bqWW00gioQZs52pnBmfZTrR05/06pmVKt/2r4JCvpW3+DjP1GtqWrYdgcj2+kiNxtK5WXw660bdhS5ou7AwkPJKN3JI970p7zWuzPBx7FnthHKaUUwu6e77oeG69lGPjrnz/7RvLifiGr8fKOF8tWG2UY+6civt+/TvN6uvl+nLhep8WyTbPaLJeTbt7zamhrsFivdsuAYslggyfRDih7gVSRwEVlvZdQnLg22y0vN20OVFOlJs2DJa/yYXy73uRv0l88uqGeLuz2P+1YmorSQVlA4saSWiCg9eCIU0QVQ0XdPUzerGfemzN55j2d4tlx7H94pk/3TAdrKJoAFdkCsTAEGyuQt1HnZG3CfPBMP/NMv+qZfuaZD5RD0hlMVgnIKA+hNM8CFsfZBm4avWXPIoUzeRbtKZpFiv9Ys0dnXqjUoTZknQ+xe2lUFmYpqoJqD6CAHjgGAV8zOpcl6qRP3bnMM6MyGW22rFCk2UuOmwvGQpOiFEKfioQ3bJRXNp7HKK/cCUYdp84jVPiLUNUFZaRGsJzbGqmMwDU58M7Z7DKHEvnULYqeCZVCbadutNCOX91YbZ+KkTzkbIzT0aZ2VHYPX34D";
             this.BindingContext = this.ViewModel;
         }
 
